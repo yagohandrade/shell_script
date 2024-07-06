c02cac5666f1eba40f9e0fee4ca75f2d953a1f75
yagohandrade <yago.andrade.hilario@gmail.com>
2024-07-06 17:36:51 -0300

    Creating url shortener with tinyurl api

--- exercise1.sh
+++ exercise1.sh
@@ -2,3 +2,22 @@
 
 # Exercise 1 shell script by Slackjeff
 
+# Use API of tinyurl for generate a short URL
+
+# curl install ?
+
+[[ $(which curl) ]] || { distro=$(grep -w "ID=.*" /etc/os-release | cut -d = -f 2); }
+
+case "$distro" in
+	ubuntu) apt update && apt upgrade && apt-get install curl ;;
+	debian) apt update && apt upgrade && apt-get install curl ;;
+
+
+# Capture user output
+read -p "Digite a url: " url
+
+[[ -z "$url"  ]] && { echo "Precisa passa uma URL..."; exit 1;}
+
+generate_url=$(curl -s https://tinyurl.com/api-create.php?url=$url)
+
+echo "Sua URL encurtada: $generate_url"
